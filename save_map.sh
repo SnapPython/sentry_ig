fe3724e68922d8d0d5829388ddbf06ee34c838a8
momo <2677785349@qq.com>
2024-05-07 20:51:32 +0800

    changed map saver dir

--- save_map.sh
+++ save_map.sh
@@ -1,6 +1,6 @@
 source install/setup.bash
-ros2 service call /SaveMap ig_lio_c_msgs/srv/SaveMap "{save_path: '/home/mechax/ch/sentry_ig', resolution: 0.0}"
+ros2 service call /SaveMap ig_lio_c_msgs/srv/SaveMap "{save_path: '$(pwd)', resolution: 0.0}"
 ros2 run nav2_map_server map_saver_cli -f new
-ros2 service call /CovertMap ig_lio_c_msgs/srv/CovertMap "{pcd_path: '/home/mechax/ch/sentry_ig/octomapfg_output.pcd',map_file_name: 'new_map'}"
+ros2 service call /CovertMap ig_lio_c_msgs/srv/CovertMap "{pcd_path: '$(pwd)/src/ig_lio_c/octomapfg_output.pcd',map_file_name: 'new_map'}"
 
 	
